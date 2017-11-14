53f9d104fab5e619652fcd01902786c62ce9c556
Brian Demant <brian.demant@gmail.com>
2017-11-14 15:07:40 +0100

    I WINgit st! :awesome:

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = require("react");
+const MyComponent_1 = require("../components/MyComponent");
+exports.default = () => (React.createElement("div", null,
+    React.createElement("h1", null, "Hello Dennis"),
+    React.createElement(MyComponent_1.default, null)));
